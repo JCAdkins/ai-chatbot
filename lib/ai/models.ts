4fafddb2f90c01767541288477225e6731cc4d39
Jordan Adkins <jordan.adkins111@gmail.com>
2024-12-10 22:28:20 -0800

    Had to make some changes to get the db connection to work through Neon Database since that's how Vercel Postgres is going to be done from now on.

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -9,17 +9,30 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
+    id: "gpt-4o-mini",
+    label: "GPT 4o mini",
+    apiIdentifier: "gpt-4o-mini",
+    description: "Small model for fast, lightweight tasks",
   },
   {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
+    id: "gpt-4o",
+    label: "GPT 4o",
+    apiIdentifier: "gpt-4o",
+    description: "For complex, multi-step tasks",
+  },
+  {
+    id: "gpt-4",
+    label: "GPT 4",
+    apiIdentifier: "gpt-4",
+    description: "Older model you get after running out of 4o requests.",
+  },
+  {
+    id: "gpt-3.5-turbo-16k",
+    label: "GPT 3.5 Turbo 16k",
+    apiIdentifier: "gpt-3.5-turbo-16k",
+    description: "Older model but infinite requests.",
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+// export const DEFAULT_MODEL_NAME: string = "gpt-4o-mini";
+export const DEFAULT_MODEL_NAME: string = "gpt-4";
